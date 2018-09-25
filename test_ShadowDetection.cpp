2f816f15560a95571a472976c80f55215cbbce8f
abderrazakiazzi <abdou.iazzi@gmail.com>
2018-09-25 21:47:54 +0100

    Update test_ShadowDetection.cpp

--- test_ShadowDetection.cpp
+++ test_ShadowDetection.cpp
@@ -9,7 +9,7 @@ void StartAffiche(){
 	std::cout << " *****    iazzi@abderrazak- @LRIT - @RIITM - @FS - @UM5 - @Rabat ************** " << std::endl;
 
 	std::cout << " // Shadow detection  " << std::endl;
-	std::cout << " // HSV et gradiant methodes" << std::endl;
+	std::cout << " // HSV and gradient methods" << std::endl;
 	std::cout << " // "<< std::endl;
 	std::cout << " // "<< std::endl;
 	std::cout << " // "<< std::endl;
@@ -22,12 +22,13 @@ void StartAffiche(){
 	 
 }
 
-void readfilevideo(/*const cv::string& file*/){
+void readfilevideo(const cv::string& file){
 		cv::VideoCapture cap;
 		//CvCapture* capt;
 		//const cv::string& file2 = "video1.mpg";
 		//capt = cvCreateFileCapture(file);
-		const cv::string& file = "F:/abdrrazak/DOCTORAT_FILE/DOC_personnelle/Base_donnee_de_chute/Coffee_room_02/video (64).avi";
+	    if(file.size() == 0)
+		const cv::string& file = "F:/myvideo.avi";	
 
 		cap.open(file);
 	    if(!cap.isOpened())
@@ -47,9 +48,6 @@ void readfilevideo(/*const cv::string& file*/){
 			  cv::waitKey(25);
 			}
 }
-
-
-
 //-----------------------------------------------------------------
 
 
@@ -58,33 +56,38 @@ int main(){
     // Debut  du programme	
 	StartAffiche();
     // debut de programme	
-	//readfilevideo();
 	ShadowDetection obj = ShadowDetection();
 
-	
+	// read two images, one is without shadows, other is with shadow
 	cv::Mat imageRef = cv::imread("imageReference.jpg");
-	cv::Mat imageCurr = cv::imread("imageCourant.jpg");
+	cv::Mat imageCurr = cv::imread("imageCurrant.jpg");
 	cv::Mat imagChro = cv::Mat(imageCurr.size(), imageCurr.depth());
 	imageCurr.copyTo(imagChro);
 
-	// HSV color step
+	// shadow detection 
 	cv::Mat res = cv::Mat(imageRef.size(), CV_8UC1);
 	obj.loadParameters();
-	//obj.detectShadowHSVColor(imageCurr, imageRef, res);
 	obj.detectShadow(imageCurr, imageRef, res);
-    obj.findNonzerosPixel(res);
-    obj.DisplayDetectPix(imageCurr);
+        obj.findNonzerosPixel(res);
+        obj.DisplayDetectPix(imageCurr);
 
-    /*// Gradient step
-	cv::Mat imresC, imresB;
-	cv::Mat res2 = cv::Mat(imageRef.size(), CV_8UC1);
-	imageRef.copyTo(imresB, res);
-	imageCurr.copyTo(imresC, res);
-	obj.detectShadowGradiant(imresB, imresC, res2, res2);	
-	cv::Mat ch = obj.chromacity(imagChro, 255, 78, "adaptive", 0, false);
-	imshow("chromacity", ch);*/
+	    /*
+	    // HSV color step 
+	    cv::Mat res = cv::Mat(imageRef.size(), CV_8UC1);
+		obj.loadParameters();
+	    obj.detectShadowHSVColor(imageCurr, imageRef, res);
+	    obj.findNonzerosPixel(res);
+	    obj.DisplayDetectPix(imageCurr);
+	    // Gradient step
+		cv::Mat imresC, imresB;
+		cv::Mat res2 = cv::Mat(imageRef.size(), CV_8UC1);
+		imageRef.copyTo(imresB, res);
+		imageCurr.copyTo(imresC, res);
+		obj.detectShadowGradiant(imresB, imresC, res2, res2);	
+		cv::Mat ch = obj.chromacity(imagChro, 255, 78, "adaptive", 0, false);
+		imshow("chromacity", ch);*/
 
-	// Display final result
+	// Display results
 	cv::imshow(" image curr", imageCurr);
 	cv::imshow("Shadow detection", res);
 	//cv::imshow("Shadow detection2", res2);
